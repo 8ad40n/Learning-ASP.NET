024da661d73fa268ef632042bb14a70ee944ddab
8ad40n <joy184110@gmail.com>
2025-03-17 04:06:04 +0600

    data pass from view to action

--- MID/Layout_ViewEngine_DataPassing/Layout_ViewEngine_DataPassing/Controllers/HomeController.cs
+++ MID/Layout_ViewEngine_DataPassing/Layout_ViewEngine_DataPassing/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Layout_ViewEngine_DataPassing.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -53,15 +54,65 @@ namespace Layout_ViewEngine_DataPassing.Controllers
         //    4. Model Binding
         public ActionResult MyPage()
         {
-            ViewBag.Message = "Your page.";
+            //ViewBag.Message = "Your page.";
 
-            // Data pass:
-            ViewBag.name = "Badhon Nath Joy";
-            ViewData["ID"] = "21-45390-3";
+            //// Data pass:
+            //ViewBag.name = "Badhon Nath Joy";
+            //ViewData["ID"] = "21-45390-3";
 
 
+            //Model binding:
+            ModelBindingTestMyPage m = new ModelBindingTestMyPage();
+            m.Name = "Badhon";
+            m.Id = 1;
+            m.university = "AIUB";
 
-            return View();
+            var m1 = new ModelBindingTestMyPage()
+            {
+                Name = "Badhon",
+                Id = 1,
+                university = "AIUB",
+            };
+            var m2 = new ModelBindingTestMyPage()
+            {
+                Name = "Badhon Nath Joy",
+                Id = 2,
+                university = "AIUB",
+            };
+
+            ModelBindingTestMyPage[] modelTest = new ModelBindingTestMyPage[] { m1, m2 };
+
+            return View(modelTest);
         }
+
+        // Model binding data pass from view to action
+        //public ActionResult MyPage2()
+        //{
+        //    ViewBag.u = Request["username"];
+        //    ViewBag.e = Request["email"];
+        //    return View();
+        //}
+
+
+        // Form collection object data pass from view to action
+        //public ActionResult MyPage2(FormCollection fc)
+        //{
+        //    ViewBag.u = fc["username"];
+        //    ViewBag.e = fc["email"];
+
+        //    return View();
+        //}
+
+
+        // Variable name mapping data pass from view to action
+        //public ActionResult MyPage2(string username, string email)
+        //{
+        //    ViewBag.u = username;
+        //    ViewBag.e = email;
+
+        //    return View();
+        //}
+
+
     }
 }
---
+++ MID/Layout_ViewEngine_DataPassing/Layout_ViewEngine_DataPassing/Models/ModelBindingTestMyPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Layout_ViewEngine_DataPassing.Models
+{
+	public class ModelBindingTestMyPage
+	{
+		public string Name { get; set; }
+		public int Id { get; set; }
+		public string university { get; set; }
+
+	}
+}
