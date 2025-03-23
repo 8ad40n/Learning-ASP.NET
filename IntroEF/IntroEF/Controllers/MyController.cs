5c008601ad3ef217585ab70c990a679a3a39609a
8ad40n <joy184110@gmail.com>
2025-03-23 21:56:13 +0600

    feat: introEF

---
+++ IntroEF/IntroEF/Controllers/MyController.cs
@@ -0,0 +1,45 @@
+﻿using IntroEF.EF;
+using IntroEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroEF.Controllers
+{
+    public class MyController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+
+            UMSEntities db = new UMSEntities();
+            var userDetail = new UserDetail
+            {
+                id = s.id,
+                name = s.name,
+                dob = s.dob,
+                email = s.email
+            };
+            db.UserDetails.Add(userDetail);
+            if (db.SaveChanges() > 0)
+            {
+                TempData["Msg"] = "User Added";
+                return RedirectToAction("Index");
+            }
+            TempData["Msg"] = "User Not Added";
+            return View();
+        }
+    }
+}
